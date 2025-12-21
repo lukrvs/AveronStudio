222537b90000575517a7c628e8c3724913606e2a
lukrvs <schoferlukas@gmail.com>
2025-12-21 04:02:16 +0100

    Update script.js

--- script.js
+++ script.js
@@ -1,1 +1,17 @@
 console.log("Website geladen!");
+
+// Lightbox-Funktion
+const cards = document.querySelectorAll('.game-card img');
+const lightbox = document.getElementById('lightbox');
+const lightboxImg = document.getElementById('lightbox-img');
+
+cards.forEach(card => {
+    card.addEventListener('click', () => {
+        lightbox.style.display = 'flex';
+        lightboxImg.src = card.src;
+    });
+});
+
+lightbox.addEventListener('click', () => {
+    lightbox.style.display = 'none';
+});
