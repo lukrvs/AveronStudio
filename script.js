b35d2b49ec595308c91c0742eafc1f070cb3ca71
lukrvs <schoferlukas@gmail.com>
2025-12-21 17:17:07 +0100

    Update script.js

--- script.js
+++ script.js
@@ -124,6 +124,8 @@ function openLogin() {
     document.body.style.overflow = 'hidden';
 }
 
+}
+
 // Schließen des Modals
 closeBtn.addEventListener('click', () => {
     loginModal.style.display = 'none';
@@ -171,5 +173,6 @@ loginForm.addEventListener('submit', (e) => {
         loginMessage.style.color = "#ff4c4c";
     }
 });
+
 
 
